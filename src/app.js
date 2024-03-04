40ee42baa6e261a5525031605b26320879dfc0d0
itumelengkolwane <162047181+katherinetumi@users.noreply.github.com>
2024-03-04 16:55:35 +0200

    update app.js

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "c8c743638tcc20ffb06eb50o53a0604d";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
